d30f67f8ba0b9c7915c5e090618386a5d9500096
rock-narwhal <1987747418@qq.com>
2024-04-10 21:34:20 +0800

    搜索>专辑页

---
+++ src/components/list/ImgWidthList.vue
@@ -0,0 +1,115 @@
+<script setup>
+import {defineProps, computed, defineEmits} from 'vue'
+
+const props = defineProps({
+  list: {
+    require: true,
+    type: Array
+  },
+  mode: {
+    default: 'ar', // ar 歌手  al 专辑  pl 歌单   dj 播客   vo  声音
+  },
+  hasMore: {
+    type: Boolean,
+    default: false
+  },
+  infinite: {
+    type: Boolean,
+    default: false
+  },
+  isLoading: {
+    type: Boolean,
+    default: false
+  },
+  coverWidth: {
+    default: 50,// 左侧封面加名称
+    type: Number
+  }
+})
+
+const disabled = computed(() => {
+  return !props.infinite || !props.hasMore || props.isLoading
+})
+const emit = defineEmits(['loadMore', 'clickItem'])
+const load = () => {
+  if (props.hasMore) {
+    emit('loadMore', props.list.length)
+  }
+}
+
+const clickItem = (id) => {
+  emit('clickItem', id)
+}
+
+const getCoverImg = (item) => {
+  let url = ''
+  switch (props.mode) {
+    case 'ar':
+      url = item.picUrl
+      break
+    case 'al':
+      url = item.picUrl
+      break
+    case 'pl':
+      url = item.coverImgUrl
+      break
+  }
+  return url + '?param=100y100'
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list"
+        :key="item.id"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+        infinite-scroll-delay="300"
+    >
+      <div @click="clickItem(item.id)" class="clearfix container">
+        <div class="img-wrap float-item" :style="{'width': `${coverWidth}%`}">
+          <img v-lazy="getCoverImg(item)" alt="">
+          {{ item.name }}
+        </div>
+        <div class="float-item clearfix" :style="{'width': `${100 - coverWidth}%`}">
+          <slot :artist="item.artist"></slot>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="less">
+@import "@/assets/less/lessDefine.less";
+ul {
+  li {
+    padding: 0 30px;
+    &:nth-child(odd) {
+      background-color: @grey57;
+    }
+  }
+}
+
+.container {
+  height: 80px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  .img-wrap {
+    height: 60px;
+    line-height: 60px;
+
+    img {
+      height: 60px;
+      margin-right: 10px;
+      vertical-align: middle;
+      border-radius: 5px;
+    }
+  }
+
+  .float-item {
+    float: left;
+  }
+}
+</style>
--- src/views/search/SearchAlbums.vue
+++ src/views/search/SearchAlbums.vue
@@ -1,11 +1,67 @@
 <script setup>
+import {ref,onMounted} from 'vue'
+import {useRoute, useRouter} from "vue-router";
+import {cloudSearch} from "@/api/api_music.js";
+import ImgWidthList from "@/components/list/ImgWidthList.vue";
 
+const isLoading = ref(false)
+
+const pageInfo = ref({
+  current: 1,
+  total: 0,
+})
+
+const queryInfo = ref({
+  offset: 0,
+  limit: 50,
+  type: 10,//专辑
+  keywords: '',
+})
+
+const list = ref([])
+const route = useRoute()
+
+onMounted(async () => {
+  let keywords = decodeURI(route.query.keywords)
+  queryInfo.value.keywords = keywords.replaceAll(/\s+/g, '+')
+  isLoading.value = true
+  const res = await cloudSearch(queryInfo.value)
+  if (res.code !== 200) return
+  list.value = res.result.albums
+  pageInfo.value.total = res.result.albumCount
+  isLoading.value = false
+})
+
+const router = useRouter()
+const toAlbumDetail = (id) => {
+  router.push({
+    name: 'AlbumDetail',
+    query: {id}
+  })
+}
 </script>
 
 <template>
-
+  <div class="album-wrap">
+    <!--  搜索 单曲页面  {{$route.query}}-->
+    <el-skeleton :rows="10" animated v-show="isLoading"></el-skeleton>
+    <ImgWidthList :list="list" mode="al" @clickItem="toAlbumDetail">
+      <template v-slot="{artist}">
+        {{ artist.name }} <span v-if="artist.trans">({{ artist.trans }})</span>
+      </template>
+    </ImgWidthList>
+  </div>
 </template>
 
 <style scoped lang="less">
-
+@import "@/assets/less/lessDefine.less";
+.album-wrap {
+  ul {
+    li {
+      &:nth-child(odd) {
+        background-color: @grey57;
+      }
+    }
+  }
+}
 </style>
