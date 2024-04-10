31b3a6c38d2626a2e88e3a63ea4b5f1d53ced3e0
rock-narwhal <1987747418@qq.com>
2024-04-10 23:32:39 +0800

    歌手详情>MV

---
+++ src/components/list/MVList.vue
@@ -0,0 +1,98 @@
+<script setup>
+import {defineProps, computed, defineEmits} from 'vue'
+import {toCNNum} from "@/utils/FormatUtil.js";
+import {timeConvert} from "@/utils/DateUtil.js";
+
+const props = defineProps({
+  list: {
+    type: Array,
+    required: true
+  },
+  infinite: {
+    type: Boolean,
+    default: false,
+  },
+  hasMore: {
+    type: Boolean,
+    default: false,
+  },
+  isLoading: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const disabled = computed(() => {
+  return !props.infinite || !props.hasMore || props.isLoading
+})
+
+const emit = defineEmits(['loadMore'])
+const load = () => {
+  if (props.hasMore) {
+    emit('loadMore', props.list.length)
+  }
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list"
+        :key="item.id"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+        infinite-scroll-delay="300"
+        class="pointer">
+      <img v-lazy="item.imgurl16v9 + '?param=600y400'" alt="">
+      <button class="play-count">
+        <svg-icon name="play-fill-white"></svg-icon>
+        {{ toCNNum(item.playCount) }}
+      </button>
+      <button class="duration">{{ timeConvert(item.duration) }}</button>
+      <div class="album-name">{{ item.name }}</div>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="less">
+ul {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  li {
+    width: 19%;
+    position: relative;
+    margin-bottom: 30px;
+
+    img {
+      border-radius: 5px;
+    }
+
+    button {
+      border: none;
+      background: transparent;
+      color: white;
+    }
+
+    .play-count {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+
+    .duration {
+      position: absolute;
+      bottom: 35px;
+      right: 10px;
+    }
+
+    .album-name {
+      height: 16px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+}
+</style>
--- src/views/detail/artist/ArtistMV.vue
+++ src/views/detail/artist/ArtistMV.vue
@@ -1,11 +1,62 @@
 <script setup>
+import {ref,reactive, onMounted} from "vue"
+import {useRoute} from "vue-router";
+import {artistMV} from "@/api/api_artist.js";
 
+const queryInfo = reactive({
+  id: '',
+  offset: 0,
+  limit: 30,
+})
+
+const hasMore = ref(false)
+
+const isLoading = ref(false)
+
+const mvList = ref([])
+
+const route = useRoute()
+onMounted(()=>{
+  queryInfo.id = route.query.id || ''
+  if(queryInfo.id){
+    mvList.value = []
+    getMVList()
+  }
+})
+
+
+const getMVList = async () =>{
+  isLoading.value = true
+  const res = await artistMV(queryInfo)
+  if (res.code !== 200) {
+    this.isLoading = false
+    return
+  }
+  mvList.value.push(...res.mvs)
+  hasMore.value = res.hasMore
+  isLoading.value = false
+}
+
+const loadMore = () =>{
+  if(!hasMore.value) return
+  queryInfo.offset = mvList.value.length
+  getMVList()
+}
 </script>
 
 <template>
-
+  <div class="mv-content">
+    <MVList :list="mvList"
+            :infinite="true"
+            :has-more="hasMore"
+            :is-loading="isLoading"
+            @loadMore="loadMore">
+    </MVList>
+  </div>
 </template>
 
 <style scoped lang="less">
-
+.mv-content {
+  padding: 0 30px
+}
 </style>
