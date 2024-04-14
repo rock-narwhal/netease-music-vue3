d592ddd3f8176f17fa10c62edcdf094cfd69b3cc
rock-narwhal <1987747418@qq.com>
2024-04-14 21:56:42 +0800

    歌曲api问题修复

--- src/api/api_music.js
+++ src/api/api_music.js
@@ -1,12 +1,12 @@
 import {get} from "@/api/request";
 /* 获取音乐url */
-export const getMusicUrl = (id) => get('/song/url', { id })
+export const getSongUrl = (id) => get('/song/url', { id , timeStamp: new Date().getTime()})
 
 /* 搜索 */
 export const cloudSearch = ({limit,offset,type,keywords}) => get('/cloudsearch', {limit, offset, type, keywords})
 
 /** 歌曲详情 */
-export const songDetail = (id) => get('/song/detail',{id})
+export const songDetail = (ids) => get('/song/detail',{ids})
 /** 专辑详情 */
 export const albumDetail = (id) => get('/album', {id})
 
