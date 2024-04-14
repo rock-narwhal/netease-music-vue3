5c5c6eb08fd962fb48f944d4440e703b1bb5a7df
rock-narwhal <1987747418@qq.com>
2024-04-14 13:56:46 +0800

    歌曲列表 tag

---
+++ src/components/commons/SongTag.vue
@@ -0,0 +1,45 @@
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({
+  tag:{
+    type: String,
+    required: true
+  },
+  color:{
+    type: String,
+    default: '#ec4141'
+  },
+  height:{
+    type: String,
+    default: '12px'
+  }
+})
+
+const styleObj = computed(() => {
+  return{
+    height: props.height,
+    lineHeight: props.height,
+    color: props.color,
+    border: '1px solid ' + props.color,
+    fontSize: '10px',
+    padding: '0 1px',
+  }
+})
+</script>
+
+<template>
+<button class="song-tag-wrapper" :style="styleObj">
+  {{tag}}<slot></slot>
+</button>
+</template>
+
+<style scoped lang="less">
+.song-tag-wrapper{
+  cursor: default;
+  outline: 0;
+  background-color: #ffffff;
+  box-sizing: border-box;
+  border-radius: 3px;
+}
+</style>
