52e4c353917719ffef66c28a0fa8f0066b07abc0
rock-narwhal <1987747418@qq.com>
2024-04-14 13:56:08 +0800

    pinia持久化 & 接口响应json

--- src/store/playStore.js
+++ src/store/playStore.js
@@ -2,30 +2,30 @@ import {defineStore} from 'pinia'
 import defaultCover from '@/assets/img/music.jpg'
 
 export const playStore = defineStore('play', {
-    state : () =>({
-           playingInfo:{
-               type : 1, // 0 表示在播放单曲，1表示在播放歌单
-               playing: false, //正在播放
-               duration: 0, //歌曲时长
-               current: 0, //当前播放时间
-               id: '', //歌曲id
-               name: '未知音乐',
-               src: '' ,//声源
-               coverUrl: defaultCover, //封面
-               lyric: '歌词',
-               album:{ //专辑
+    state: () => ({
+        playingInfo: {
+            type: 1, // 0 表示在播放单曲，1表示在播放歌单
+            playing: false, //正在播放
+            duration: 0, //歌曲时长
+            current: 0, //当前播放时间
+            id: '', //歌曲id
+            name: '未知音乐',
+            src: '',//声源
+            coverUrl: defaultCover, //封面
+            lyric: '歌词',
+            album: { //专辑
 
-               },
-               artists:[{
-                   name:'未知歌手'
-               }],
+            },
+            artists: [{
+                name: '未知歌手'
+            }],
 
-           },
-            // 播放列表
-            playlist:[]
+        },
+        // 播放列表
+        playlist: []
     }),
-    actions:{
-        updatePlayingInfo(song){
+    actions: {
+        updatePlayingInfo(song) {
             this.playingInfo.id = song.id
             this.playingInfo.name = song.name
             this.playingInfo.coverUrl = song.coverUrl
@@ -36,8 +36,8 @@ export const playStore = defineStore('play', {
             this.playingInfo.album = song.album
             this.playingInfo.artists = song.artists
         },
-        updatePlayList(song){
-            if(this.playlist.find(item => item.id = song.id)){
+        updatePlayList(song) {
+            if (this.playlist.find(item => item.id = song.id)) {
                 return
             }
             this.playlist.unshift(song)
@@ -46,5 +46,15 @@ export const playStore = defineStore('play', {
         updatePlaylistAll(list) {
             this.playList = list
         }
+    },
+    persist: { //持久化到localStorage
+        enabled: true,
+        strategies: [
+            {
+                key: 'playStore',
+                storage: localStorage
+            },
+
+        ]
     }
 })
