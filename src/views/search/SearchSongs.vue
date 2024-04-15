a016fa607be863eb267f3bb600a937dba83d1333
rock-narwhal <1987747418@qq.com>
2024-04-15 20:39:43 +0800

    搜索>歌曲 添加按钮

--- src/views/search/SearchSongs.vue
+++ src/views/search/SearchSongs.vue
@@ -4,6 +4,7 @@ import {useRoute} from "vue-router";
 import {cloudSearch} from "@/api/api_music.js";
 import MusicList from "@/components/list/MusicList.vue";
 import emitter from '@/utils/MittBus.js'
+import SvgIcon from "@/components/svg/SvgIcon.vue";
 
 const isLoading = ref(false)
 const pageInfo = ref({
@@ -51,6 +52,17 @@ const playMusic = (id) => {
 <template>
   <div>
     <!--  搜索 单曲页面  {{$route.query}}-->
+    <div class="mar-side-30 mar-bot-10">
+      <button class="cir-btn-red pointer font-14 mar-ri-10">
+        <svg-icon name="play-fill" class-name="font-20" vertical="-0.25"></svg-icon>
+        播放全部
+      </button>
+
+      <button class="cir-btn-white pointer font-14 dark-color">
+        <svg-icon name="download-one" class-name="font-20" vertical="-0.25"></svg-icon>
+        下载全部
+      </button>
+    </div>
     <el-skeleton :rows="10" animated v-show="isLoading"></el-skeleton>
     <MusicList v-show="!isLoading" style="margin:0 30px" :data-list="songList" @clickItem="playMusic"></MusicList>
   </div>
